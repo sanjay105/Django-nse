03a22f1e21c0b1ba7bc0f3905008611f963e3c31
sanjay <sanjayreddy.banda@gmail.com>
2018-09-15 14:58:58 +0530

    initial commit

--- nse/views.py
+++ nse/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import stocksData
+from django.template import loader
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def showgraph(request):
+    stklist=stocksData.objects.all()
+    #print(stklist)
+    template = loader.get_template('nse/index.html')
+    context = {
+        'stock_list': stklist,
+    }
+    return HttpResponse(template.render(context, request))
